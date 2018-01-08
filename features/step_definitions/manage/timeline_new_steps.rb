e0043ab4d9c2d49ad7651154c531b6c166d6fe71
phiku <philipp.kuederli@zhdk.ch>
2018-01-08 10:15:24 +0100

    backup

--- features/step_definitions/manage/timeline_new_steps.rb
+++ features/step_definitions/manage/timeline_new_steps.rb
@@ -4,13 +4,12 @@ def user_by_login(login)
   User.find_by(login: login)
 end
 
-def create_pool(login, name, description)
+def create_pool(name, description)
   ip3 = FactoryGirl.create(:inventory_pool, name: name, description: description)
-  FactoryGirl.create(:access_right, user: user_by_login(login), inventory_pool: ip3, role: :inventory_manager)
 end
 
-def create_model(product_name)
-  FactoryGirl.create(:model, product: product_name)
+def create_model(product_name, maintenance_period)
+  FactoryGirl.create(:model, product: product_name, maintenance_period: maintenance_period)
 end
 
 def model_by_product_name(product_name)
@@ -52,11 +51,33 @@ def open_timeline(inventory_pool_name, product_name)
   binding.pry
 end
 
+def create_reservation(login, ip_name)
+  u = user_by_login(login)
+  ip = inventory_pool_by_name(ip_name)
+  FactoryGirl.create(:reservation, user: u, start_date: Date.today, end_date: Date.tomorrow, inventory_pool: ip)
+end
+
+def add_ip_manager(ip_name, login)
+  u = user_by_login(login)
+  ip = inventory_pool_by_name(ip_name)
+  FactoryGirl.create(:access_right, user: u, inventory_pool: ip, role: :inventory_manager)
+end
+
+def add_ip_customer(ip_name, login)
+  u = user_by_login(login)
+  ip = inventory_pool_by_name(ip_name)
+  FactoryGirl.create(:access_right, user: u, inventory_pool: ip, role: :customer)
+end
+
 def execute_scenario
   create_a_user('User1')
-  create_pool('User1', 'Ip3', 'Description3')
-  create_model('Model1')
+  create_a_user('User2')
+  create_pool('Ip3', 'Description3')
+  add_ip_manager('Ip3', 'User1')
+  add_ip_customer('Ip3', 'User2')
+  create_model('Model1', 3)
   create_item('Ip3', 'Model1', 'Inv1')
+  create_reservation('User2', 'Ip3')
   do_login('User1', 'password') # Default password in user_factory.rb
   open_timeline('Ip3', 'Model1')
 end
