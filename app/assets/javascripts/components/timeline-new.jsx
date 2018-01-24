9072f59783fc9ceb6ef27c3605ad99dd28bffaee
phiku <philipp.kuederli@zhdk.ch>
2018-01-24 08:41:44 +0100

    new simple algorithm

--- app/assets/javascripts/components/timeline-new.jsx
+++ app/assets/javascripts/components/timeline-new.jsx
@@ -984,6 +984,170 @@
 
     },
 
+
+    hasPendingBooking(booking) {
+      return this.pendingBooking(booking) != null
+    },
+
+    pendingBooking(booking) {
+      return _.find(booking, (b) => b.assignment == null)
+    },
+
+    assignedBookings(booking) {
+      return _.filter(booking, (b) => b.assignment != null)
+    },
+
+    trySimpleAssign(current, leftovers) {
+
+      _.each(
+        current.entitlementGroupIds,
+        (egid) => {
+          if(current.assignment == null && leftovers[egid] != undefined && leftovers[egid] > 0) {
+            current.assignment = egid
+            leftovers[egid]--
+          }
+        }
+      )
+    },
+
+    tryModify(current, assignedBooking, leftovers) {
+
+      var currentAssignment = assignedBooking.assignment
+      var candidates = _.difference(
+        assignedBooking.entitlementGroupIds,
+        currentAssignment
+      )
+      candidates = _.difference(
+        candidates,
+        current.entitlementGroupIds
+      )
+
+      var candidate = _.find(
+        candidates,
+        (egid) => {
+          if(leftovers[egid] != undefined && leftovers[egid] > 0) {
+          }
+        }
+      )
+
+      if(candidate) {
+        leftovers[assignedBooking.assignment]++
+        leftovers[candidate]--
+        assignedBooking.assignment = candidate
+      }
+    },
+
+    findCandidateGroupId(current, assignedBooking, leftovers) {
+
+    },
+
+    modifyAnAssignedBooking(current, assignedBookings, leftovers) {
+
+      _.each(
+        assignedBookings,
+        (ab) => {
+          if(current.assignment == null) {
+            this.tryModify(current, ab, leftovers)
+          }
+        }
+
+      )
+
+    },
+
+    assignBooking(current, assignedBookings, leftovers) {
+
+      this.trySimpleAssign(current, leftovers)
+      if(current.assignment == null) {
+        this.modifyAnAssignedBooking(current, assignedBookings, leftovers)
+      }
+      if(current.assignment == null) {
+        current.assignment = ''
+      }
+
+    },
+
+    newAlgorithm(reservations, constraints) {
+
+      var leftovers = _.clone(constraints)
+      var booking = _.map(reservations, (entitlementGroupIds, reservationId) => {
+        return {
+          reservationId: reservationId,
+          assignment: null,
+          entitlementGroupIds: entitlementGroupIds
+        }
+      })
+
+      while(this.hasPendingBooking(booking)) {
+        var current = this.pendingBooking(booking)
+        var assignedBookings = this.assignedBookings(booking)
+        this.assignBooking(
+          current,
+          assignedBookings,
+          leftovers
+        )
+
+      }
+
+      return booking
+
+    },
+
+
+    newAlgorithmForReservations(timeline_availability, reservationsList, userEntitlementGroupsForModel, relevantItemsCount) {
+
+      var before = performance.now()
+
+      var reservations = _.object(reservationsList.map((r) => {
+        return [
+          r.id,
+          this.reservationEntitlements(timeline_availability, r, userEntitlementGroupsForModel).concat([''])
+        ]
+      }))
+
+      var constraints = _.object(
+        timeline_availability.entitlements.map((e) => {
+          return [
+            e.entitlement_group_id,
+            e.quantity
+          ]
+        }).concat([
+          [
+            '',
+            relevantItemsCount - _.reduce(
+              timeline_availability.entitlements,
+              (memo, e) => memo + e.quantity,
+              0
+            )
+          ]
+        ])
+      )
+
+      // var onlyGeneralCount = candidates.length - candidates2.length
+      //
+      // var result = true
+      // if(candidates2.length > 0) {
+      result = this.newAlgorithm(reservations, constraints)
+      // } else {
+      //
+      //   // if(onlyGeneralCount)
+      //   result = {
+      //     result: 'all-from-general'
+      //
+      //   }
+      // }
+      // else {
+      //   return true
+      // }
+
+      var after = performance.now();
+
+      console.log('delta = ' + (after - before))
+
+      return result
+
+    },
+
     findEntitlementCombination(timeline_availability, dayIndex, userEntitlementGroupsForModel, relevantItemsCount) {
 
       var day = moment().add(dayIndex, 'days')
@@ -1393,7 +1557,7 @@
           change: c,
           date: c,
           reservations: reservations,
-          algorithm: this.algorithmForReservations(timeline_availability, reservations, userEntitlementGroupsForModel, relevantItemsCount),
+          algorithm: this.newAlgorithmForReservations(timeline_availability, reservations, userEntitlementGroupsForModel, relevantItemsCount),
           available: relevantItemsCount - _.size(reservations)
         }
       })
