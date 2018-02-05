3ac38792400994786ca12d8a4425fccf3ed1ee9c
phiku <philipp.kuederli@zhdk.ch>
2018-02-05 16:06:52 +0100

    display editable fields

--- engines/leihs_admin/app/assets/javascripts/leihs_admin/components/FieldEditor.jsx
+++ engines/leihs_admin/app/assets/javascripts/leihs_admin/components/FieldEditor.jsx
@@ -32,17 +32,56 @@
       )
     },
 
+    renderTitle() {
+      return (
+        <div className='panel'>
+          <div className='row'>
+            <div className='col-sm-6'>
+              <h1>Feld Editor</h1>
+            </div>
+          </div>
+        </div>
+      )
+    },
+
+    renderEditableFields() {
+      return this.editableFields().map(f => {
+        return (
+          <div key={'editable_field_' + f.id} className='row' style={{wordBreak: 'break-word', paddingTop: '15px', paddingBottom: '15px'}}>
+            <div className='col-sm-4'>
+              <strong>
+                {f.data.label}
+              </strong>
+            </div>
+            <div className='col-sm-8'>
+              {JSON.stringify(f)}
+            </div>
+          </div>
+        )
+      })
+    },
+
+    renderEditableFieldsBox() {
+      return (
+        <div className='panel panel-default'>
+          <div className='panel-heading'>
+            <h4>Editierbare Felder</h4>
+          </div>
+          <div className='panel-body'>
+            <div className='list-of-lines'>
+              {this.renderEditableFields()}
+            </div>
+          </div>
+        </div>
+      )
+    },
+
     render () {
 
       return (
         <div>
-          <div className='panel'>
-            <div className='row'>
-              <div className='col-sm-6'>
-                <h1>Feld Editor</h1>
-              </div>
-            </div>
-          </div>
+          {this.renderTitle()}
+          {this.renderEditableFieldsBox()}
         </div>
       )
     }
