69f766c3a11a3d51954769e91f9a86d5bee3382d
phiku <philipp.kuederli@zhdk.ch>
2018-02-19 14:28:01 +0100

    fix rubo

--- spec/steps/manage/availability_calendar_steps.rb
+++ spec/steps/manage/availability_calendar_steps.rb
@@ -80,7 +80,8 @@ module Manage
         expect(numbers_col_txt).to eq label
       end
 
-      step 'the timeline shows :assigned assigned of :available available for the group' do |assigned, available|
+      step 'the timeline shows :as assigned of :av available for the group' \
+        do |as, av|
         @order_line ||= find('#lines .order-line', text: @model.product)
         within(@order_line.find('.multibutton')) do
            find('.dropdown-holder').click
@@ -88,8 +89,15 @@ module Manage
         end
         within('.modal.in') do
           within_frame 'timeline' do
-            find("div[title='Entitlement Info #{@group.id}']", text: "#{available} reserviert für Gruppe #{@group.name}, davon zugewiesen")
-            expect(find("div[title='Entitlement #{@group.id}']").text.start_with?(assigned)).to eq(true)
+            find(
+              "div[title='Entitlement Info #{@group.id}']",
+              text: "#{av} reserviert für Gruppe #{@group.name}"\
+                    ', davon zugewiesen'
+            )
+            expect(
+              find("div[title='Entitlement #{@group.id}']")
+                .text.start_with?(as)
+            ).to eq(true)
           end
           click_on _('Close')
         end
